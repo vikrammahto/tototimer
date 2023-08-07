b4e3a44ea6fc3d34eff37fc7c353990d4f6ce13d
vikrammahto <vikramkr.in@gmail.com>
2023-08-07 18:53:51 +0530

    Added react-router-dom and weather route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Todo from './components/Todo';
 import { TodoProvider } from './context/TodoContext';
 import Header from './layout/Header';
 import SidebarMenu from './layout/SidebarMenu';
+import Weather from './components/Weather';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen mx-auto lg:border sm:max-w-xl md:max-w-full lg:max-w-screen-xl ">
-      <Header className="sticky top-0 z-50" />
-      <div className="flex flex-1overflow-y-hidden">
-        <SidebarMenu className="sticky top-0 z-50" />
-        <main className="flex-1 px-5 overflow-y-auto">
-          <TodoProvider>
-            <Todo />
-          </TodoProvider>
-        </main>
+    <Router>
+      <div className="flex flex-col h-screen mx-auto lg:border sm:max-w-xl md:max-w-full lg:max-w-screen-xl ">
+        <Header className="sticky top-0 z-50" />
+        <div className="flex flex-1overflow-y-hidden">
+          <SidebarMenu className="sticky top-0 z-50" />
+          <main className="flex-1 px-5 overflow-y-auto">
+            <TodoProvider>
+              <Routes>
+                <Route path="/" element={<Todo />} />
+                <Route path="/weather" element={<Weather />} />
+              </Routes>
+            </TodoProvider>
+          </main>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Weather.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <div>Weather</div>
+  )
+}
+
+export default Weather
