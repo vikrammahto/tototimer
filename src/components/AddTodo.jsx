cd2513f1d6ba53005ef5219df0d4ab46a38fe3a5
vikrammahto <vikramkr.in@gmail.com>
2023-08-07 12:26:41 +0530

    Fixed JSX error

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -5,18 +5,18 @@ const AddTodo = () => {
   const { text, handleTextChange, handleSubmit } = useContext(TodoContext);
   return (
     <form className="flex flex-col mb-3" onSubmit={handleSubmit}>
-      <div class="relative border border-gray-300 rounded-lg">
+      <div className="relative border border-gray-300 rounded-lg">
         <input
           type="text"
           value={text}
           onChange={handleTextChange}
-          class="h-14 w-100 rounded-lg pl-3 z-0 focus:outline-none border-none focus:ring-0"
+          className="z-0 pl-3 border-none rounded-lg h-14 w-100 focus:outline-none focus:ring-0"
           placeholder="Eg: Prepare weekly schedule"
         />
-        <div class="absolute top-2 right-2">
+        <div className="absolute top-2 right-2">
           <button
             type="submit"
-            class="h-10 w-20 text-white rounded-lg bg-sky-500 hover:bg-sky-700 focus:bg-sky-500 focus:rounded-lg"
+            className="w-20 h-10 text-white rounded-lg bg-sky-500 hover:bg-sky-700 focus:bg-sky-500 focus:rounded-lg"
           >
             Add
           </button>
