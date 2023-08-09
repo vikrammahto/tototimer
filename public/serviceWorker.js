abc6ed76c9c2c6a0b1fd5f2fb20704ccef6497f7
vikrammahto <vikramkr.in@gmail.com>
2023-08-09 11:07:42 +0530

    Fix #1 PWA issue

---
+++ public/serviceWorker.js
@@ -0,0 +1,44 @@
+/* eslint-env serviceWorker */
+
+const CACHE_NAME = 'tototimer-cache-v1';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function (response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response as it can only be consumed once
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
