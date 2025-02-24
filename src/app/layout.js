dec2804b817eed2fcc93d444cd6578eaf3d815ff
vikrammahto <vikramkr.in@gmail.com>
2025-02-24 16:16:48 +0530

    Refactor globals.css and add Inter font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,19 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  weight: '400',
+  subsets: ['latin'],
+});
+
 export const metadata = {
   title: 'React App',
   description: 'Web site created with Next.js.',
 };
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <div id="root">{children}</div>
-      </body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
   );
 }
